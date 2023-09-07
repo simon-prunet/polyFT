490ccefe6e75908865b40b668727eed0b9d2225d
Simon Prunet <prunet@gmail.com>
2023-09-07 15:48:05 +0200

    Debug (Alpha was gamma_n - gamma_n-1 instead of gamma_n+1 - gamma_n) and added analytical formula for hexagonal mask transform

--- poly.py
+++ poly.py
@@ -25,7 +25,8 @@ class polyFT:
 		self.Gamma = Gamma
 		self.npoints = Gamma.shape[0]
 		# Compute normalized polygone edges \alpha_n
-		self.Alpha = Gamma - np.roll(Gamma,1,axis=0)
+		#self.Alpha = Gamma - np.roll(Gamma,1,axis=0)
+		self.Alpha = np.roll(Gamma,-1,axis=0) - Gamma
 		self.Alpha /= np.linalg.norm(self.Alpha,axis=1)[:,None]
 		# Also keep shifted Alpha matrix handy
 		self.Alpha_m1 = np.roll(self.Alpha,1,axis=0)
@@ -49,3 +50,21 @@ class polyFT:
 
 
 
+def hexagon_coordinates(R=1.0):
+	'''
+	computes coordinates of hexagon vertices.
+	R is outer circle radius
+	'''
+	r = np.sqrt(3.)/2. * R # inner radius
+	Gamma = np.array([[R,0.],[R/2,r],[-R/2,r],[-R,0.],[-R/2,-r],[R/2,-r]])
+	return (Gamma)
+
+def hexagon_transform(W,R=1.0):
+	'''
+	computes FT of hexagon mask of outer radius R.
+	'''
+	u = 2.*np.pi * W[:,0]
+	v = 2.*np.pi * W[:,1]
+	s3 = np.sqrt(3.)
+	calc = -4*s3/(u+s3*v)/(u-s3*v)*np.cos(u*R) + 2.*s3/u/(u+s3*v)*np.cos(0.5*u*R-s3/2.*v*R) + 2.*s3/u/(u-s3*v)*np.cos(u/2*R+s3/2*v*R)
+	return(calc)
