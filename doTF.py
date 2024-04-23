891132f06cf3c164a527841f9fd43dde489c1a59
Simon Prunet <simon.prunet@oca.eu>
2024-04-23 11:30:30 +0200

    Polygonal FT with timings

--- doTF.py
+++ doTF.py
@@ -1,15 +1,19 @@
 from poly import *
 from fresnel import diffraction
 import numpy as np
+from timer import Timer
 
 # Create NW2 profile
-n = 2000
+n = 8000
 print('Creating petal with %d points'%(n*2*24))
 p = petal_FT(n_border=n,profile_type='sister',profile_path='Matlab_files/NW2.mat')
 
 # Create diffraction instance
-m = 2**13
+m = 2**10
 diff = diffraction(p,m)
 
+t = Timer()
 print('Computing polygonal mask transform')
-diff.compute_polygonal_fmask()
+t.start()
+diff.compute_polygonal_fmask(gpu_memory_limit=30)
+t.stop()
