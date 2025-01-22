61481c1b9af3395e51f40a6127aec935764900c0
Simon Prunet <prunet@gmail.com>
2025-01-22 09:02:10 +0000

    Changed to trapeze profile.

--- doit.py
+++ doit.py
@@ -5,10 +5,11 @@ import numpy as np
 # Choose GPU
 cp.cuda.Device(2).use()
 # Create NW2 profile
-n = 2000
-print('Creating petal with %d points'%(n*2*24))
+n_border = 2000
+n_petals = 24
+print('Creating petal with %d points'%(n_petals*2*24))
 #p = petal_FT(n_border=n,profile_type='sister',profile_path='Matlab_files/NW2.mat')
-p = petal_FT(n_border=n,profile_type='trapeze',profile_path='Matlab_files/Param_PourOc.mat')
+p = petal_FT(n_petals=n_petals, n_border=n_border,profile_type='trapeze',profile_path='Matlab_files/Param_PourOc.mat')
 
 # Create diffraction instance
 m = 2**13
@@ -25,9 +26,9 @@ diff.compute_diffraction_patterns()
 # Save polygonal mask and diffraction patterns
 print('Saving mask transform')
 #np.save('/data101/prunet/fmask_n%d_m%d'%(n,m),diff.polygonal_fmask)
-np.save('/scratch/prunet/polyFT/trapeze/fmask_n%d_m%d'%(n,m),diff.polygonal_fmask)
+np.save('/scratch/prunet/polyFT/trapeze/fmask_n%d_m%d'%(n_petals,m),diff.polygonal_fmask)
 # Save diffraction patterns
 print('Saving diffraction patterns')
 #np.save('/data101/prunet/diffraction_n%d_m%d'%(n,m),diff.diffracted)
-np.save('/scratch/prunet/polyFT/trapeze/diffraction_n%d_m%d'%(n,m),diff.diffracted)
+np.save('/scratch/prunet/polyFT/trapeze/diffraction_n%d_m%d'%(n_petals,m),diff.diffracted)
 
